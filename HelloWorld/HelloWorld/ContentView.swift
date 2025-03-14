be15eaac882104e4b348e51fc5aeeae26ebf6c1f
Antônio Druzo Rocha Neto <druzo@terra.com.br>
2025-03-14 06:53:25 -0300

    Implementação de links no projeto de hello World.

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Antonio Druzo Rocha Neto on 14/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Link("Go to Apple", destination: URL(string: "https://apple.com")!)
+            Link("Go to Google", destination: URL(string: "https://google.com")!)
+                .buttonStyle(.bordered)
+            Link("Send email", destination: URL(string: "mailto:mail@mail.com")!)
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.buttonBorder)
+                .controlSize(.large)
+                .tint(.indigo)
+            
+            Link(destination: URL("https://apple.com")!) {
+                HStack(spacing: 16) {
+                    Image(systemName: "apple.logo")
+                    Text("Apple Store")
+                }
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(
+                    Capsule()
+                        .fill(.blue)
+                        .shadow(radius: 20)
+                )
+            }
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Antonio Druzo Rocha Neto on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
