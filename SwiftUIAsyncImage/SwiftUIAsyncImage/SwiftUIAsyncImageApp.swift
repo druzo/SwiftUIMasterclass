3f461a9f79748632efc9ec4ba08dbe5471363268
Antônio Druzo Rocha Neto <druzo@terra.com.br>
2025-03-14 08:45:15 -0300

    Finalização projeto exemplo AsyncImage

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Antonio Druzo Rocha Neto on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
