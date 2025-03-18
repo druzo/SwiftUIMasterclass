c051d958dc3a25b270d2447d669ee5eddf243f38
Antônio Druzo Rocha Neto <idd_arocha@uolinc.com>
2025-03-18 16:08:12 -0300

    Setup do projeto
    Confugurações iniciais do projeto com os assets e os ícones.

---
+++ Hike/Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Antonio Druzo Rocha Neto on 18/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Antonio Druzo Rocha Neto on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
